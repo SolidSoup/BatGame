d7290af9efb0fbfffa9662d99ba5bfb7c4224827
WSquared88 <westleywaligora@gmail.com>
2014-04-10 20:21:39 -0400

    Quadtangles added
    added the quadtangles class. It doesnt do much right now. SORRY KAREN.

---
+++ BatGame/BatGame/BatGame/QuadTangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace BatGame
+{
+    class QuadTangle : Rectangle
+    {
+        Rectangle tLeft;
+        Rectangle tRight;
+        Rectangle bLeft;
+        Rectangle bRight;
+
+        public QuadTangle(int x, int y, int width, int height)
+        {
+            tLeft = new Rectangle(x, y, width / 2, height / 2);
+            tRight = new Rectangle(x + (width / 2), y, width / 2, height / 2);
+            bLeft = new Rectangle(x, y + (width / 2), width / 2, height / 2);
+            bRight = new Rectangle(x + (width / 2), y + (width / 2), width / 2, height / 2);
+        }
+    }
+}
